d9f190ebafac0c9c096bfdff9aa3f1ff57e3ea5d
Ubinquitous <ubinquitous1@gmail.com>
2022-10-08 17:11:21 +0900

    ADD : 영화 이미지 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
-import RouterBar from './RouterBar';
+import React, { useEffect, useState } from 'react';
+import SEO from '../components/SEO';
+import { movieData } from '../types/movieData';
 
-const Haha = () => {
+const Index = () => {
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = () => {
+        (async () => {
+            const data = await (await fetch(`/api/movies`)).json();
+            setMovies(data.boxOfficeResult.weeklyBoxOfficeList)
+            console.log(data.boxOfficeResult.weeklyBoxOfficeList);
+        })();
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-            <h1>Hello</h1>
-            <RouterBar />
+            <SEO>Home</SEO>
+            {!movies && <h4>Loading</h4>}
+            <div className='movie-wrap'>
+                <div className='movie-box'>
+                    {movies.map((movie: movieData) => (
+                        <div key={movie.rnum}>
+                            <picture>
+                                <img src='https://www.kgnews.co.kr/data/photos/20220835/art_1661933622713_f74df6.jpg' alt='영화이미지' />
+                            </picture>
+                            <br />
+                            <span>{movie.movieNm.length >= 15 ? `${movie.movieNm.slice(0, 15)}...` : movie.movieNm}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <style jsx>{`
+                .movie-wrap {
+                    display: grid;
+                    place-items:center;
+                }
+
+                .movie-box {
+                    display: grid;
+                    grid-template-columns: repeat(5, minmax(100px, 1fr));
+                    grid-gap: 20px;
+                    width: 80%;
+                    padding-top: 30px;
+                    place-items: center;
+                }
+
+                img {
+                    border-radius: 4px;
+                    width: 200px;
+                    height: auto;
+                }
+            `}</style>
         </div>
     );
 };
 
-export default Haha;
+export default Index;
