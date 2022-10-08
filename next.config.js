1b6d6593853a193f8f73f65c256fe8974fc05b4f
Ubinquitous <ubinquitous1@gmail.com>
2022-10-08 17:13:03 +0900

    ADD : rewrites destination

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
+const API_KEY = `f5eef3421c602c6cb7ea224104795888&targetDt=20120101`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/redirect/:path*',
+        destination: 'newRedirect/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=${API_KEY}`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
